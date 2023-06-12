1f920f8774cb354035a5a748e862fd3012295a44
Larry-Lobster-Studio <100290519+Larry-Lobster-Studio@users.noreply.github.com>
2023-06-12 02:08:02 +0200

    Update searchbar.tsx

--- admin/src/app/(home)/components/navigation/searchbar.tsx
+++ admin/src/app/(home)/components/navigation/searchbar.tsx
@@ -1,32 +1,112 @@
 'use client';
 
-import clsx from 'clsx';
-import { MagnifyingGlass } from '@phosphor-icons/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Calculator, Calendar, Settings, Smile, User } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
+import {
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+	CommandShortcut,
+} from '@/components/command';
+import { Button } from '@/components/button';
 
 export default function Searchbar() {
+	const { push } = useRouter();
+	const [open, setOpen] = useState(false);
+
+	useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault();
+				setOpen((open) => !open);
+			}
+		};
+
+		document.addEventListener('keydown', down);
+		return () => document.removeEventListener('keydown', down);
+	}, []);
+
 	return (
-		<div className={clsx('w-full max-w-xl')}>
-			<label htmlFor='search' className={clsx('sr-only')}>
-				Search
-			</label>
-			<div className={clsx('relative w-full')}>
-				<MagnifyingGlass
-					size={26}
-					className={clsx(
-						'absolute inset-y-0 left-0 h-full pl-3 text-flyer-gray/60 pointer-events-none'
+		<>
+			<Button
+				variant={'outline'}
+				className={cn('relative text-flyer-gray', 'sm:pr-20')}
+				onClick={() => setOpen((open) => !open)}
+			>
+				<span className={cn('hidden', 'lg:inline-flex')}>
+					Search invoices, clients...
+				</span>
+
+				<kbd
+					className={cn(
+						'absolute hidden items-center gap-1 rounded border bg-muted right-1.5 top-2 h-5 px-1.5 pointer-events-none select-none',
+						'sm:flex'
 					)}
-				/>
-				<input
-					id='search'
-					type='search'
-					placeholder='Search invoices, clients...'
-					required
-					className={clsx(
-						'block w-full p-2 pl-8 text-sm rounded-lg border border-lg border-flyer-gray/30 bg-flyer-white',
-						'focus:outline-none focus:border-flyer-primary focus:ring-flyer-primary'
-					)}
-				/>
-			</div>
-		</div>
+				>
+					<span className={cn('text-xs')}>⌘</span> K
+				</kbd>
+			</Button>
+			<CommandDialog open={open} onOpenChange={setOpen}>
+				<CommandInput placeholder='Search invoices, clients...' />
+				<CommandList>
+					<CommandEmpty>No results found.</CommandEmpty>
+					<CommandGroup heading='Suggestions'>
+						<CommandItem>
+							<Calendar className='mr-2 h-4 w-4' />
+							<span>Calendar</span>
+						</CommandItem>
+						<CommandItem>
+							<Smile className='mr-2 h-4 w-4' />
+							<span>Search Emoji</span>
+						</CommandItem>
+						<CommandItem>
+							<Calculator className='mr-2 h-4 w-4' />
+							<span>Calculator</span>
+						</CommandItem>
+					</CommandGroup>
+					<CommandSeparator />
+					<CommandGroup heading='Settings'>
+						<CommandItem>
+							<User className='mr-2 h-4 w-4' />
+							<span>Profile</span>
+							<CommandShortcut
+								tabIndex={0}
+								onKeyDown={(e) => {
+									if (e.key === 'p' && (e.metaKey || e.ctrlKey)) {
+										e.preventDefault();
+										push('/settings');
+									}
+								}}
+							>
+								⌘P
+							</CommandShortcut>
+						</CommandItem>
+						<CommandItem>
+							<Settings className='mr-2 h-4 w-4' />
+							<span>Settings</span>
+							<CommandShortcut
+								tabIndex={0}
+								onKeyDown={(e) => {
+									if (e.key === 's' && (e.metaKey || e.ctrlKey)) {
+										e.preventDefault();
+										push('/settings');
+									}
+								}}
+							>
+								⌘S
+							</CommandShortcut>
+						</CommandItem>
+					</CommandGroup>
+				</CommandList>
+			</CommandDialog>
+		</>
 	);
 }
